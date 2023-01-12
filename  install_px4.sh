92412324b39745adb4b36f105c77548108f61ae9
fbenti <benti97@hotmail.it>
2023-01-12 14:08:22 +0100

    add px4 installation

---
+++  install_px4.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/PX4/PX4-Autopilot/main/Tools/setup/ubuntu.sh
+wget https://raw.githubusercontent.com/PX4/PX4-Autopilot/main/Tools/setup/requirements.txt
+bash ubuntu.sh --no-nuttx --no-sim-tools
