57dba9cd3cee2c043b9cc52ca74cab57b128bb53
fbenti <benti97@hotmail.it>
2023-01-17 18:02:44 +0100

    added docu and rtps installation

---
+++ install_ros2_ros_com_1_13.sh
@@ -0,0 +1,7 @@
+mkdir -p ~/px4_ros_com_ros2/src
+cd ~/px4_ros_com_ros2/src
+
+git clone -b release/1.13 https://github.com/PX4/px4_ros_com.git ~/px4_ros_com_ros2/src/px4_ros_com
+git clone -b release/1.13 https://github.com/PX4/px4_msgs.git ~/px4_ros_com_ros2/src/px4_msgs
+cd ~/px4_ros_com_ros2/src/px4_ros_com/scripts
+./build_ros2_workspace.bash
