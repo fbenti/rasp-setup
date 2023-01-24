1f4177b0e241fde2b0c04d16a04e6326bd5a1ca3
fbenti <benti97@hotmail.it>
2023-01-24 15:11:20 +0100

    modified foonathan

--- install_foonathan.sh
+++ install_foonathan.sh
@@ -1,5 +1,6 @@
+cd
 git clone https://github.com/eProsima/foonathan_memory_vendor.git
 cd foonathan_memory_vendor
 mkdir build && cd build
 cmake ..
-cmake --build . --target install
+sudo cmake --build . --target install
