c082db20540fc3718f802144ea2386a280e3f99f
fbenti <benti97@hotmail.it>
2023-01-25 20:56:16 +0100

    fixed path

--- install_Fast_RTPS_Gen.sh
+++ install_Fast_RTPS_Gen.sh
@@ -1,13 +1,9 @@
 #clone Fast-RTPS-Gen 1.0.4:
-
-#git clone --recursive https://github.com/eProsima/Fast-DDS-Gen.git -b v1.0.4 ~/Fast-RTPS-Gen \
-#&& cd ~/Fast-RTPS-Gen/gradle/wrapper
+git clone --recursive https://github.com/eProsima/Fast-DDS-Gen.git -b v1.0.4 ~/Fast-RTPS-Gen 
 
 # After that, modify the distribution version of gradle inside the gradle-wrapper.properties file to gradle-6.8.3 such that the distributionUrl file becomes as follows:
+sed -i 's/5.6.2/6.8.3/g' ~/Fast-RTPS-Gen/gradle/wrapper/gradle-wrapper.properties 
 
-#distributionUrl=https\://services.gradle.org/distributions/gradle-6.8.3-bin.zip
-
-# Now you should run the following commands:
-
+# compile
 cd ~/Fast-RTPS-Gen 
 ./gradlew assemble && sudo env "PATH=$PATH" ./gradlew install
--- install_foonathan.sh
+++ install_foonathan.sh
@@ -1,6 +1,6 @@
 cd
-git clone https://github.com/eProsima/foonathan_memory_vendor.git
-cd foonathan_memory_vendor
+git clone https://github.com/eProsima/foonathan_memory_vendor.git ~/foonathan_memory_vendor
+cd ~/foonathan_memory_vendor
 mkdir build && cd build
 cmake ..
 sudo cmake --build . --target install
