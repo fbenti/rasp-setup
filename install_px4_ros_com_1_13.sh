07f867d90c5096637caa7eb12aa298f61c889725
fbenti <benti97@hotmail.it>
2023-01-30 09:48:50 +0100

    added source workspace

--- install_px4_ros_com_1_13.sh
+++ install_px4_ros_com_1_13.sh
@@ -5,3 +5,5 @@ git clone -b release/1.13 https://github.com/PX4/px4_ros_com.git ~/px4_ros_com_r
 git clone -b release/1.13 https://github.com/PX4/px4_msgs.git ~/px4_ros_com_ros2/src/px4_msgs
 cd ~/px4_ros_com_ros2/src/px4_ros_com/scripts
 ./build_ros2_workspace.bash
+
+echo 'source /px4_ros_com/install/setup.bash' | sudo tee --append ~/.profile
