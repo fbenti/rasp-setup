c7f91174529c661796eeb4a282c33e0a9bab6eb5
fbenti <benti97@hotmail.it>
2023-01-30 12:41:44 +0100

    modified source to bashrc

--- install_all.sh
+++ install_all.sh
@@ -8,4 +8,4 @@ bash install_foonathan.sh
 bash install_Fast_RTPS_Gen.sh
 
 # px4_ros_com - v.1.13 - with rtps
-install_px4_ros_com_1_13.sh
+bash install_px4_ros_com_1_13.sh
--- install_px4_ros_com_1_13.sh
+++ install_px4_ros_com_1_13.sh
@@ -4,6 +4,7 @@ cd ~/px4_ros_com_ros2/src
 git clone -b release/1.13 https://github.com/PX4/px4_ros_com.git ~/px4_ros_com_ros2/src/px4_ros_com
 git clone -b release/1.13 https://github.com/PX4/px4_msgs.git ~/px4_ros_com_ros2/src/px4_msgs
 cd ~/px4_ros_com_ros2/src/px4_ros_com/scripts
-bash build_ros2_workspace.bash
+source build_ros2_workspace.bash
 
-echo 'source ~/px4_ros_com/install/setup.bash' | sudo tee --append ~/.bashrc
+echo 'source ~/px4_ros_com_ros2/install/setup.bash' | sudo tee --append ~/.bashrc
+reboot
