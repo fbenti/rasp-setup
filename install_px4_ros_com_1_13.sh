1bc9160f8657c62f3d53a6528da2ca60d128f4d6
fbenti <benti97@hotmail.it>
2023-01-30 11:13:04 +0100

    modified source to bashrc

--- install_px4_ros_com_1_13.sh
+++ install_px4_ros_com_1_13.sh
@@ -4,6 +4,6 @@ cd ~/px4_ros_com_ros2/src
 git clone -b release/1.13 https://github.com/PX4/px4_ros_com.git ~/px4_ros_com_ros2/src/px4_ros_com
 git clone -b release/1.13 https://github.com/PX4/px4_msgs.git ~/px4_ros_com_ros2/src/px4_msgs
 cd ~/px4_ros_com_ros2/src/px4_ros_com/scripts
-./build_ros2_workspace.bash
+bash build_ros2_workspace.bash
 
-echo 'source /px4_ros_com/install/setup.bash' | sudo tee --append ~/.profile
+echo 'source /px4_ros_com/install/setup.bash' | sudo tee --append ~/.bashrc
